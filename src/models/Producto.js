f1b7fc759749b9f9285f33f4a0dcd9ac42cf6c50
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-19 03:49:04 -0400

    agregando datos pertinentes al producto

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -2,17 +2,21 @@ export default class Producto {
   constructor(
     id = 0,
     nombre = "",
-    fotos = [],
+    imagen = [],
     descripcion = "",
     rating = 0,
+    stock = 0,
+    cantEvaluado = 0,
     precio = 0,
     descuento = 0
   ) {
     (this.id = id),
     (this.nombre = nombre),
-    (this.fotos = fotos),
+    (this.imagen = imagen),
     (this.descripcion = descripcion),
     (this.rating = rating),
+    (this.stock = stock),
+    (this.cantEvaluado = cantEvaluado),
     (this.precio = precio),
     (this.descuento = descuento);
   }

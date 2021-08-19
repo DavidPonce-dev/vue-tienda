b5afd0b0b7b9e4778511bf81695ca45c9e56f0f6
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-19 03:49:15 -0400

    generando view grafica de los datos

--- src/components/Producto.vue
+++ src/components/Producto.vue
@@ -1,19 +1,29 @@
 <template>
   <v-card elevation="2" max-width="374" class="mx-auto my-md-12 my-sm-5">
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    />
+    <v-carousel height="250" v-if="typeof producto.imagen === 'object'">
+      <v-carousel-item v-for="(imagen, i) in producto.imagen" :key="i">
+        <v-sheet color="primary" height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <v-img height="100%" :src="imagen" />
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
 
-    <v-card-title
-      >Cafe Badilico
-      <div class="ms-auto text-subtitle-1">25 en stock</div>
+    <v-img v-else height="250" :src="producto.imagen" />
+
+    <v-card-title>
+      {{ producto.nombre }}
+      <div v-if="producto.stock > 0" class="ms-auto text-subtitle-1">
+        {{ producto.stock }} en stock}
+      </div>
+      <div v-else class="ms-auto text-subtitle-1">no hay stock</div>
     </v-card-title>
 
     <v-card-text>
       <v-row align="center" class="mx-0">
         <v-rating
-          :value="4.5"
+          :value="producto.rating"
           color="amber"
           dense
           half-increments
@@ -21,15 +31,17 @@
           size="14"
         ></v-rating>
 
-        <div class="grey--text ms-4">4.5 (413)</div>
+        <div class="grey--text ms-4">{{ producto.rating }} ({{producto.cantEvaluado}})</div>
       </v-row>
 
-      <div class="mt-4 text-subtitle-1">$ 2.500</div>
-      <div class="mb-4 text-subtitle-1">$ 2.000</div>
-
-      <div>
-        Small plates, salads & sandwiches - an intimate setting with 12 indoor
-        seats plus patio seating.
+      <div
+        class="mt-4 text-subtitle-1"
+        :class="producto.descuento > 0 ? 'descuento-aplicado' : ''"
+      >
+        $ {{ producto.precio }}
+      </div>
+      <div v-if="producto.descuento > 0" class="mb-4 text-subtitle-1">
+        $ {{ producto.precio - producto.descuento }}
       </div>
     </v-card-text>
     <v-card-actions>
@@ -38,6 +50,7 @@
         color="primary"
         rounded
         @click="addProducto(producto)"
+        :disabled="producto.stock <= 0"
       >
         Agregar al carrito
       </v-btn>
@@ -59,5 +72,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.descuento-aplicado {
+  color: red;
+  text-decoration: line-through;
+}
 </style>

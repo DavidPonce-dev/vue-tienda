29579cc4e28d91277323b5102a36a41305593808
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-19 00:10:44 -0400

    agregando metodos de addProducto

--- src/store/cart.js
+++ src/store/cart.js
@@ -2,5 +2,15 @@ export default {
     namespaced: true,
     state: {
         productos: []
+    },
+    mutations:{
+        addProducto (state, producto) {
+            state.productos.push(producto)
+        }
+    },
+    actions:{
+        addProducto ({commit}, producto) {
+            commit('addProducto', producto)
+        }
     }
 }

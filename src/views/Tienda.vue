7bc708585da25be86eb51af12cd4584724613e0e
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-19 00:10:06 -0400

    vista de Tienda

---
+++ src/views/Tienda.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <h1>Vista Tienda</h1>
+    <v-row>
+      <v-col
+        v-for="producto in productos"
+        :key="producto.id"  
+        md="4"
+      >
+        <Producto :producto="producto" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Producto from "../components/Producto.vue";
+export default {
+  components: {
+    Producto,
+  },
+  computed: {
+    ...mapState('tienda', ['productos'])
+  }
+};
+</script>

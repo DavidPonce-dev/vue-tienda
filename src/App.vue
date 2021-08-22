ef7953b9886356878dfcf302a2db2ddd93ff8a98
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-22 03:17:03 -0400

    agregando gradiente

--- src/App.vue
+++ src/App.vue
@@ -18,3 +18,10 @@ export default {
   }
 };
 </script>
+
+<style>
+.v-application--wrap{
+  background: rgb(161,161,161);
+background: linear-gradient(29deg, rgba(161,161,161,1) 0%, rgba(255,255,255,1) 66%, rgba(255,255,255,1) 78%, rgba(143,143,143,1) 100%);
+}
+</style>

75183e27c1b811aa567bc9ddba78112f0172c6bd
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-22 00:41:07 -0400

    fix

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -1,5 +1,5 @@
 export default class Producto {
-  constructor(
+  constructor({
     id = 0,
     nombre = "",
     imagen = [],
@@ -8,16 +8,16 @@ export default class Producto {
     stock = 0,
     cantEvaluado = 0,
     precio = 0,
-    descuento = 0
-  ) {
+    descuento = 0,
+  }) {
     (this.id = id),
-    (this.nombre = nombre),
-    (this.imagen = imagen),
-    (this.descripcion = descripcion),
-    (this.rating = rating),
-    (this.stock = stock),
-    (this.cantEvaluado = cantEvaluado),
-    (this.precio = precio),
-    (this.descuento = descuento);
+      (this.nombre = nombre),
+      (this.imagen = imagen),
+      (this.descripcion = descripcion),
+      (this.rating = rating),
+      (this.stock = stock),
+      (this.cantEvaluado = cantEvaluado),
+      (this.precio = precio),
+      (this.descuento = descuento);
   }
 }

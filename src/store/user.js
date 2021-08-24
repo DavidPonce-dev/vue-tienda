5afee553c3126f5debc75e49cba5f40083a5e18c
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-23 20:34:15 -0400

    separando en servicios las llamadas fetch

--- src/store/user.js
+++ src/store/user.js
@@ -1,52 +1,4 @@
-//pseudo llamada a un servidor que no existe
-const userList = [
-  {
-    email: "asd@asd.asd",
-    password: "asdasdasd",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3JyZW8iOiJhc2RAYXNkLmFzZCIsIm5hbWUiOiJKaG9uIFNtaXRoIn0.sbFoY_efcpAm1DgLGuLmC04RGayffKtIxQUrHhp-IDU",
-  },
-  {
-    email: "qwe@asd.asd",
-    password: "qweqweqwe",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3JyZW8iOiJxd2VAYXNkLmFzZCIsIm5hbWUiOiJxd2Vxd2UifQ.qCPyUQq_9pXOzi9qUwprQmlkn88wnIFBe_OVE-RRoK8",
-  },
-];
-
-const runLogin = ({ email, password }) => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      for (const userOfList of userList) {
-        if (userOfList.email == email && userOfList.password == password) {
-          res({ email: userOfList.email, token: userOfList.token });
-        }
-        rej(new Error("Error en los datos ingresados"));
-      }
-    }, 1000);
-  });
-};
-
-const runRegister = () => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res();
-    }, 1000);
-  });
-};
-
-const fetchToken = (token) => {
-  return new Promise((res, rej) => {
-    for (const userOfList of userList) {
-      if (userOfList.token == token) {
-        res({ email: userOfList.email, token: userOfList.token });
-      }
-    }
-    rej(new Error("La sesion ha expirado"));
-  });
-};
-
-//desde aqui el codigo "real"
+import { loginSvc, registerSvc, checkTokenSvc } from '../services/user'
 import User from "../models/User.js";
 
 export default {
@@ -61,17 +13,17 @@ export default {
   },
   actions: {
     async login({ commit }, data) {
-      const user = await runLogin(data);
+      const user = await loginSvc(data);
       const logged = new User(user);
       commit("setUser", logged);
       return user.token;
     },
     async loadToken({ commit }, token) {
-      const user = await fetchToken(token);
+      const user = await checkTokenSvc(token);
       commit("setUser", user);
     },
     async register() {
-      await runRegister();
+      await registerSvc();
     },
     async logout({ commit }) {
       localStorage.removeItem("token");

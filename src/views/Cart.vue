d83cf8e55f98bf8c3359eae21ca1b6d3e51f00e6
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-26 06:52:44 -0400

    generando base de vista del carrito

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,9 +1,39 @@
 <template>
   <v-container>
-    <h1>Vista Carrito</h1>
+    <v-row>
+      <v-col cols="8">
+        <ProductoCarrito
+          v-for="producto in productos"
+          :key="producto.id"
+          :producto="producto"
+        />
+      </v-col>
+      <v-col cols="4">
+        <v-card>
+          <v-card-title primary-title> Resumen del pedido </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+import ProductoCarrito from "../components/ProductoCarrito";
+export default {
+  components: {
+    ProductoCarrito,
+  },
+  data() {
+    return {
+      productos: [],
+    };
+  },
+  computed: {
+    ...mapState("cart", ["carrito"]),
+  },
+  mounted() {
+    this.productos = this.carrito.filter((producto) => producto != undefined);
+  },
+};
 </script>

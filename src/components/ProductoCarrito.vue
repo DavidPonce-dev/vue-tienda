9e3a32a9c49b71f06fedb07c18fe6a18706b08e5
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-26 06:52:57 -0400

    creando componente para productos en el carrito

---
+++ src/components/ProductoCarrito.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="mb-4">
+    <v-card-title primary-title>
+      {{ producto.nombre }}
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    producto: {
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>

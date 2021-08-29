b6e1f13e290b1c9b1825c09968701a41d20f57bd
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-28 22:53:31 -0400

    agregando mutacion y accion remToCart

--- src/store/cart.js
+++ src/store/cart.js
@@ -10,17 +10,32 @@ export default {
       const { id } = producto
 
       if (hasOwnProperty.call(carrito, id)) {
-        carrito[id].cantidad += 1
+        carrito[id].cantidad ++
       } else {
         producto.cantidad = 1
         carrito[id] = producto;
       }
       state.carrito = [...carrito]
     },
+    remToCart(state, producto) {
+      const { carrito } = state;
+      const { id } = producto
+
+      if (carrito[id].cantidad > 1){
+        carrito[id].cantidad --
+      }else{
+        delete carrito[id]
+      }
+
+      state.carrito = [...carrito]
+    },
   },
   actions: {
     addToCart({ commit }, producto) {
       commit("addToCart", producto);
+    },
+    remToCart({ commit }, producto) {
+      commit("remToCart", producto);
     },
   },
 };
